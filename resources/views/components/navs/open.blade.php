934f82c9fd6138b1f16cb04a5eae2eaa48e37e51
Hamdan Yuwafi <yuwafi.hamdan365@gmail.com>
2021-05-12 04:12:56 +0700

    Adds Links to New Route

--- resources/views/components/navs/open.blade.php
+++ resources/views/components/navs/open.blade.php
@@ -1,13 +1,19 @@
 <nav class="white">
     <div class="nav-wrapper container">
         <a href="{{ route('landing-page') }}" class="brand-logo left black-text">
-            <img src="{{ asset('img/logo-apmfi.png') }}" alt="APMFI" height="60">
+            <img src="{{ asset('img/logo.png') }}" alt="Mitseda" height="60">
         </a>
         
         <ul id="nav-mobile" class="right">
             <li {{ ($page == "login")? 'class=active': '' }}>
                 <a href="{{ route('login') }}" class="black-text">Masuk</a>
             </li>
+
+            @if (Route::has('register'))
+                <li {{ ($page == "register")? 'class=active': '' }}>
+                    <a href="{{ route('register') }}" class="black-text">Daftar</a>
+                </li>
+            @endif
         </ul>
     </div>
 </nav>
