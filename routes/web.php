2204405b7bf27fda96c92f276131402a6fc50945
Hamdan Yuwafi <yuwafi.hamdan365@gmail.com>
2021-05-11 23:58:25 +0700

    Add New Routes

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,10 @@ Route::view('/', 'pages.landing.home')->name('landing-page');
 Route::view('tutorial', 'pages.landing.tutorial')->name('landing-tutorial');
 
 Route::middleware('guest')->group(function () {
-    Route::get('login', [SessionCtrl::class, 'create'])->name('login');
+    Route::view('login', 'pages.login')->name('login');
+    Route::post('login', [SessionCtrl::class, 'create'])->name('login');
+    Route::view('register', 'pages.register')->name('register');
+    Route::post('register', [SessionCtrl::class, 'store'])->name('register');
 });
 
 Route::prefix('praktikum')->group(function () {
@@ -33,9 +36,14 @@ Route::prefix('praktikum')->group(function () {
 
     Route::prefix('registrasi')->group(function () {
         Route::get('/', [PracticeRegistrationCtrl::class, 'index'])->name('register-new-practice');
-        Route::get('pilih-lab', [PracticeRegistrationCtrl::class, 'timeAndPlace'])->name('register-set-time-and-place');
-        Route::get('isi-data', [PracticeRegistrationCtrl::class, 'fillFormData'])->name('register-fill-practice-data');
-        Route::get('ringkasan', [PracticeRegistrationCtrl::class, 'formSummary'])->name('register-get-form-summary');
+        Route::get('isi-data', [PracticeRegistrationCtrl::class, 'timeAndPlace'])->name('register-set-time-and-place');
+        Route::post('isi-data', [PracticeRegistrationCtrl::class, 'timeAndPlaceResponse'])->name('register-set-time-and-place');
+        Route::get('isi-data/kti', [PracticeRegistrationCtrl::class, 'fillKTIFormData'])->name('register-fill-kti-practice-data');
+        Route::get('isi-data/ext', [PracticeRegistrationCtrl::class, 'fillEXTFormData'])->name('register-fill-ext-practice-data');
+        Route::get('isi-data/reg', [PracticeRegistrationCtrl::class, 'fillREGFormData'])->name('register-fill-reg-practice-data');
+        Route::post('isi-data/kti', [PracticeRegistrationCtrl::class, 'storeKTIForm'])->name('register-fill-kti-practice-data');
+        Route::post('isi-data/ext', [PracticeRegistrationCtrl::class, 'storeEXTForm'])->name('register-fill-ext-practice-data');
+        Route::post('isi-data/reg', [PracticeRegistrationCtrl::class, 'storeREGForm'])->name('register-fill-reg-practice-data');
         Route::get('tiket-praktikum', [PracticeRegistrationCtrl::class, 'practiceTicket'])->name('register-get-practice-ticket');
     });
 
@@ -49,7 +57,7 @@ Route::prefix('praktikum')->group(function () {
 Route::prefix('admin')->middleware('auth')->group(function () {
     Route::get('/', [UserCtrl::class, 'index'])->name('home');
 
-    Route::get('logout', [SessionCtrl::class, 'destroy'])->name('admin-logout');
+    Route::get('logout', [SessionCtrl::class, 'destroy'])->name('logout');
 
     Route::get('form-baru', [NewFormCtrl::class, 'index'])->name('admin-new-practices');
     Route::get('form-baru/{id}', [NewFormCtrl::class, 'show'])->name('admin-new-practice');
@@ -66,6 +74,12 @@ Route::prefix('admin')->middleware('auth')->group(function () {
     Route::get('riwayat-praktikum', [PracticeHistoryCtrl::class, 'index'])->name('admin-practice-histories');
     Route::get('riwayat-praktikum/{id}', [PracticeHistoryCtrl::class, 'show'])->name('admin-practice-history');
     Route::get('riwayat-praktikum/{id}/print', [PracticeHistoryCtrl::class, 'print'])->name('admin-practice-history-print');
+
+    Route::get('laboratorium', [ToolsClassificationCtrl::class, 'index'])->name('admin-manage-laboratories');
+    Route::get('laboratorium/tambah', [ToolsClassificationCtrl::class, 'create'])->name('admin-add-laboratory');
+    Route::get('laboratorium/{id}', [ToolsClassificationCtrl::class, 'show'])->name('admin-show-laboratory');
+    Route::get('laboratorium/{id}/edit', [ToolsClassificationCtrl::class, 'edit'])->name('admin-edit-laboratory');
+    Route::get('laboratorium/{id}/hapus', [ToolsClassificationCtrl::class, 'destroy'])->name('admin-delete-laboratory');
 
     Route::get('alat', [ToolsClassificationCtrl::class, 'index'])->name('admin-manage-tools');
     Route::get('alat/tambah', [ToolsClassificationCtrl::class, 'create'])->name('admin-add-tool');
