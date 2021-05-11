b6ff0d306100bf8ac4379c7fd128d6a8a14221dc
Hamdan Yuwafi <yuwafi.hamdan365@gmail.com>
2021-05-12 04:16:23 +0700

    Adds Business Logic For Login and Register Process

--- app/Http/Controllers/SessionCtrl.php
+++ app/Http/Controllers/SessionCtrl.php
@@ -3,17 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class SessionCtrl extends Controller
 {
-    public function create()
-    {
-        return view('pages.login');
-    }
-
-    public function store(LoginRequest $request)
+    public function create(LoginRequest $request)
     {
         $data = $request->validated();
 
@@ -24,6 +22,25 @@ class SessionCtrl extends Controller
         
         $request->session()->flash('msgfailed', 'Kombinasi email dan password tidak ditemukan.');
         return redirect()->route('login');
+    }
+
+    public function store(RegisterRequest $request)
+    {
+        $data = (object) $request->validated();
+
+        $user = new User();
+        $user->name = $data->name;
+        $user->email = $data->email;
+        $user->password = Hash::make($data->password);
+        $isSaved = $user->save();
+
+        if ($isSaved) {
+            $request->session()->flash('msgsuccess', 'Akun baru berhasil dibuat. Silakan login menggunakan akun tersebut.');
+            return redirect()->route('login');
+        }
+
+        $request->session()->flash('msgfailed', 'Akun baru gagal dibuat.');
+        return redirect()->route('register')->withInput((array) $data);
     }
 
     public function destroy(Request $request)
